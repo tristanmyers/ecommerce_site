6db1bc9ecfd812d201c4012dbf50b14714e03d88
Tristan Myers <tristanmyers.developer@gmail.com>
2021-12-31 12:34:05 -0500

    added a form for adding item to cart, added function to push form data to sessionstorage

--- src/routes/ProductPage.js
+++ src/routes/ProductPage.js
@@ -3,7 +3,10 @@ import { useSearchParams } from "react-router-dom";
 import Header from "../components/Header.js";
 import { products } from "../index.js";
 
+export const cartStorage = window.sessionStorage;
+
 export default function ProductPage(props) {
+	// show product corresponding to the id in the url
 	let [searchParams] = useSearchParams();
 	let itemId = searchParams.get("itemId");
 	return (
@@ -14,9 +17,22 @@ export default function ProductPage(props) {
 					<img src={products[itemId]["itemImage"]} alt="black and white sneaker" width="auto" height="400px"/>
 					<p>{products[itemId]["itemDesc"]}</p>
 					<p><strong>${products[itemId]["itemPrice"]}</strong></p>
+					<form id="add-to-cart-form" name="add-to-cart-form">
+						<fieldset>
+							<select name="number-of-items" id="number-of-items-select" defaultValue="one">
+								<option value="quantity of items" disabled>--Quantity of this item--</option>
+								<option value="1">1</option>
+								<option value="2">2</option>
+								<option value="3">3</option>
+								<option value="4">4</option>
+							</select>
+						</fieldset>
+					</form>
 					<button
-						className="product-add-button"
+						id="add-to-cart-button"
+						name="add-to-cart-button"
 						type="button"
+						onClick={() => addToCart(itemId)}
 					>
 						Add to cart
 						<svg
@@ -30,14 +46,12 @@ export default function ProductPage(props) {
 							<path d="M11 9h2V6h3V4h-3V1h-2v3H8v2h3v3zm-4 9c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zm10 0c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2zm-8.9-5h7.45c.75 0 1.41-.41 1.75-1.03l3.86-7.01L19.42 4l-3.87 7H8.53L4.27 2H1v2h2l3.6 7.59-1.35 2.44C4.52 15.37 5.48 17 7 17h12v-2H7l1.1-2z" />
 						</svg>
 					</button>
-					<select name="number-of-items" id="number-of-items-select">
-						<option value="quantity of items">--Quantity of this item--</option>
-						<option value="one">1</option>
-						<option value="two">2</option>
-						<option value="three">3</option>
-						<option value="four">4</option>
-					</select>
 				</section>
 		</div>
 	);
+}
+
+function addToCart(id) {
+	let itemQuantity = document.querySelector("#number-of-items-select").selectedOptions[0].value;
+	cartStorage.setItem("itemsInCart", JSON.stringify({itemId: id, quantity: itemQuantity}));
 }
