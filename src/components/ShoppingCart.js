81ec2b728926f160f5a8c5f35973298b41e9ae6b
tristanmyers <tristanmyers.developer@gmail.com>
2022-01-03 17:17:29 -0500

    added new cart counter

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-const cartStorage = window.sessionStorage;
-const items = JSON.parse(cartStorage.getItem("itemsInCart"));
-console.log(items)
-
+// TODO: rerender when itemsInCart has been update.
 export default class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.cartStorage = window.sessionStorage;
+    this.storedItems = JSON.parse(this.cartStorage.getItem("itemsInCart"));
+    this.state = {
+      numberOfItems: this.storedItems === null ? 0 : this.storedItems.length
+    }
+  }
   render() {
     return (
       <div className="shopping-cart-container">
@@ -19,7 +24,7 @@ export default class ShoppingCart extends React.Component {
             <path d="M0 0h24v24H0V0z" fill="none" />
             <path d="M15.55 13c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.37-.66-.11-1.48-.87-1.48H5.21l-.94-2H1v2h2l3.6 7.59-1.35 2.44C4.52 15.37 5.48 17 7 17h12v-2H7l1.1-2h7.45zM6.16 6h12.15l-2.76 5H8.53L6.16 6zM7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zm10 0c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z" />
           </svg>
-          <p className="cart-counter"></p>
+          <p className="cart-counter">{this.state.numberOfItems}</p>
         </a>
       </div>
     );
