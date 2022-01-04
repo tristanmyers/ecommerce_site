9bcad86614e7596c2e3d641b8d58cf3cff44ef0e
Tristan Myers <tristanmyers.developer@gmail.com>
2022-01-04 09:53:24 -0500

    made logo a link to homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import ShoppingCart from "./ShoppingCart.js";
 export default function Header(props) {
   return (
     <div className="header-container">
-      <h1 className="logo">Awesome Shoes</h1>
+      <h1 className="logo"><a href="/">Awesome Shoes</a></h1>
       <ShoppingCart />
     </div>
   );
