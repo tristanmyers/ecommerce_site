b5b873e27d3c9d5aa735a641a70eb1878215d4d0
Tristan Myers <tristanmyers.developer@gmail.com>
2022-01-16 15:44:59 -0500

    now using store hooks to remove item from cart

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,10 +1,10 @@
 import React from "react"
-
-const cartStorage = window.sessionStorage;
-const items = JSON.parse(cartStorage.getItem("itemsInCart"));
+import { useSelector, useDispatch } from "react-redux";
+import { removeItemFromCart } from "../cartSlice.js"
 
 // TODO: rerender after item has been removed
 export default function ShoppingCartItem(props) {
+	const dispatch = useDispatch();
 	return (
 		<div className="flex flex-row gap-4 pb-2 bg-white rounded cart-item-container h-1/3">
 			<div className="flex flex-row items-center gap-5 px-2 py-4">
@@ -15,20 +15,9 @@ export default function ShoppingCartItem(props) {
 			<div className="flex flex-row items-center">
 				<button className="p-2 border border-black hover:bg-grey cart-remove-button"
 				type="button" 
-				onClick={() => removeItemFromCart(props.itemId)}>
+				onClick={() => dispatch(removeItemFromCart(props.itemId))}>
 				Remove item</button>
 			</div>
 		</div>
 	);
-} 
-
-function removeItemFromCart(id) {
-	items.map((item, index) => {
-		if (id === item.itemId) {
-			items.splice(index, 1);
-			return cartStorage.setItem("itemsInCart", JSON.stringify(items));
-		}	else {
-			return console.log("no item to remove");
-		}
-	})
 }
