690c8fd1d4bd1d848f88cc793ccf14d006c0d20e
Tristan Myers <tristanmyers.developer@gmail.com>
2022-04-12 21:24:18 -0400

    shopping cart page responsive

--- src/pages/ShoppingCartPage.js
+++ src/pages/ShoppingCartPage.js
@@ -5,30 +5,32 @@ import Footer from '../components/Footer.js'
 import { products } from "../index.js";
 import ShoppingCartItem from "../components/ShoppingCartItem.js";
 
-
-export default function ShoppingCartPage() {
+// getting ids for available products, for each item check if the id matches an id in availableProductIds if so render the ShoppingCartItem component.
+function RenderItemsInCart() {
 	const items = useSelector((state) => state.cart.itemsInCart);
 	
-	// getting ids for available products, for each item check if the id matches an id in availableProductIds if so render the ShoppingCartItem component.
-	function renderItemsInCart() {
-		const availableProductsIds = products.map((product, index) => products[index].itemId);
+	const availableProductsIds = products.map((product, index) => products[index].itemId);
 
-		if (items === null || items.length === 0) {
-			return <p>No items in cart</p>
-		} else {
-			return items.map((item, index) => {
-				if (Number(item.itemId) === availableProductsIds[item.itemId]) {
-					return <ShoppingCartItem key={index} itemId={item.itemId} itemName={products[item.itemId].itemName} itemQuantity={items[index].quantity} itemImage={products[item.itemId]["itemImage"]} />
-				} else {
-					return <p>Item not available</p>
-				}
-			})
-		}
+	if (items === null || items.length === 0) {
+		return <p>No items in cart</p>
+	} else {
+		return items.map((item, index) => {
+			if (Number(item.itemId) === availableProductsIds[item.itemId]) {
+				return <ShoppingCartItem key={index} itemId={item.itemId} itemName={products[item.itemId].itemName} itemQuantity={items[index].quantity} itemImage={products[item.itemId]["itemImage"]} itemPrice={products[item.itemId]["itemPrice"]}/>
+			} else {
+				return <p>Item not available</p>
+			}
+		})
 	}
+}
+
+export default function ShoppingCartPage() {
+	
+	
 	return (
     <div className="flex flex-col h-screen gap-1 font-light bg-main-background text-main-font">
       <Header />
-			{renderItemsInCart()}
+			<RenderItemsInCart />
 			<Footer />
 		</div>
 	);
